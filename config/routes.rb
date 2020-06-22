019f6b5c4e5f881c97d2013d0f4909032e49976d
Kenji, Shirase <juvenile.games0630@gmail.com>
2020-06-22 19:29:19 +0900

    add user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root 'homes#top'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }  
+  resources :users, only: [:show]
 
   resources :books, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
