33407a40f6014e479d6674a26f7f4405049dc0bb
Kenji, Shirase <juvenile.games0630@gmail.com>
2020-06-23 12:26:04 +0900

    add books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,45 @@
 class BooksController < ApplicationController
-def new
-end
-def create
-end
-def index
-end
-def show
-end
+  def create
+    @book = Book.new(post_book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id);
+  end
+
+  def index
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @b = Book.find(params[:id])
+    @b.update(post_book_params)
+    @book = Book.new
+    redirect_to book_path(params[:id])
+  end
+
+  def destroy
+    @b = Book.find(params[:id])
+    @b.destroy
+    redirect_to books_path
+  end
+
+  def show
+    @book = Book.new
+    @b = Book.find(params[:id])
+    @user = User.find(@b.user_id)
+    @books = Book.all
+  end
+
+private
+  def post_book_params
+    logger.info "title:#{:title}"
+    logger.info "body:#{:body}"
+    params.require(:book).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }  
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show, :index, :edit, :update]
 
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
